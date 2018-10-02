0db69d82b76660c2bf1832aa7b2ec31488cc54c2
David Steinich <dsteinich@flexion.us>
2018-10-02 09:07:42 -0500

    Fix project name

--- transformActivity.sql
+++ transformActivity.sql
@@ -12,8 +12,7 @@ insert /*+ append parallel(4) */ into wqx_activity_project (act_uid, project_id_
 select /*+ parallel(4) */
        activity_project.act_uid,
        listagg(project.prj_id, ';') within group (order by project.prj_id) project_id_list,
-       listagg(project.prj_id, ';') within group (order by project.prj_id) project_name_list
---//TODO - values too long       listagg(project.prj_name, ';') within group (order by project.prj_id) project_name_list
+       rtrim(clobagg(project.prj_name || '; '), '; ') project_name_list
   from wqx.activity_project
        left join wqx.project
          on activity_project.prj_uid = project.prj_uid
