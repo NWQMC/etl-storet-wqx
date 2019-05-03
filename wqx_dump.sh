f85ea0d0be4fd0814b187a0fb48ca0e9e827d5dc
David Uselmann <duselmann@contractor.usgs.gov>
2019-05-03 11:40:51 -0500

    updated epa url domain to www3.epa.gov
    There might be a redirect from www.epa.gov to www3.epa.gov that is causing ETL fails.

--- wqx_dump.sh
+++ wqx_dump.sh
@@ -98,7 +98,7 @@ grep -q "successfully completed" ${EXPORT_LOG} || (echo "${EXPORT_LOG} does not 
 comm -13 <(grep -o ${DUMP_FILE_GREP} ${EXPORT_LOG}) <(ls | grep ${CLEAN_UP_GREP}) | xargs rm -f
 
 # download any dump files newer on remote than they are on local
-grep -o ${DUMP_FILE_GREP} ${EXPORT_LOG} | sed -e 's/^/http:\/\/www.epa.gov\/storet\/download\/storetw\//' | xargs -n 1 -P 12 wget -Nq
+grep -o ${DUMP_FILE_GREP} ${EXPORT_LOG} | sed -e 's/^/http:\/\/www3.epa.gov\/storet\/download\/storetw\//' | xargs -n 1 -P 12 wget -Nq
 
 # we have new data downloaded, so export the rundate
 stop_with_job_id
